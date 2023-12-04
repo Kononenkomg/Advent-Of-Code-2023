31f2933170510f8b9c6ab364350a44923ac22561
Maksym Kononenko <kononenkomg@gmail.com>
2023-12-03 18:56:19 -0800

    day 3 second part

--- day3.ts
+++ day3.ts
@@ -48,6 +48,9 @@ type checkIfNumberOnTheLeftInput = {
 
 function checkIfNumberOnTheLeft(input: checkIfNumberOnTheLeftInput) {
   const result = [];
+  if (!isNaN(parseInt(input.row.split("")[input.index]))) {
+    result.push(input.index);
+  }
   if (!isNaN(parseInt(input.row.split("")[input.index - 1]))) {
     result.push(input.index - 1);
     result.push(
@@ -59,12 +62,48 @@ function checkIfNumberOnTheLeft(input: checkIfNumberOnTheLeftInput) {
 
 function checkIfNumberOnTheRight(input: checkIfNumberOnTheLeftInput) {
   const result = [];
+  if (!isNaN(parseInt(input.row.split("")[input.index]))) {
+    result.push(input.index);
+  }
   if (!isNaN(parseInt(input.row.split("")[input.index + 1]))) {
     result.push(input.index + 1);
     result.push(
       ...checkIfNumberOnTheRight({ row: input.row, index: input.index + 1 })
     );
   }
+  return result;
+}
+
+function removeDuplicatesAndSort(arr: number[]) {
+  return [...Array.from(new Set<number>(arr.sort((a, b) => a - b)))];
+}
+
+function mapSubArraysOfIndexes(arr: number[]) {
+  let subarrays = [];
+  let currentSubarray = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (Math.abs(arr[i] - currentSubarray[currentSubarray.length - 1]) === 1) {
+      currentSubarray.push(arr[i]);
+    } else {
+      subarrays.push(currentSubarray);
+      currentSubarray = [arr[i]];
+    }
+  }
+
+  subarrays.push(currentSubarray);
+  return subarrays;
+}
+
+function mapNumbersFromArray(indexes: number[][], arr: string[]) {
+  const result = [];
+  indexes.forEach((subarray) => {
+    let str = "";
+    subarray.forEach((element) => {
+      str += arr[element];
+    });
+    result.push(parseInt(str));
+  });
   return result;
 }
 
@@ -76,60 +115,65 @@ function main() {
     const arr = data.split("\n");
     let result = 0;
     for (let i = 0; i < arr.length; i++) {
-      console.log(arr[i]);
 
       const upRow = arr[i - 1];
       const middleRow = arr[i];
       const downRow = arr[i + 1];
-      const arrayOfIndexesWhereNumber = [];
       middleRow.split("").forEach((element, i) => {
-        if (!isNaN(parseInt(element))) {
-          if (
-            checkIfNumberTouchingSymbol({
-              upRow,
-              middleRow,
-              downRow,
-              index: i,
-            })
-          ) {
-            arrayOfIndexesWhereNumber.push(i);
-            arrayOfIndexesWhereNumber.push(
-              ...checkIfNumberOnTheLeft({ row: middleRow, index: i })
-            );
-            arrayOfIndexesWhereNumber.push(
-              ...checkIfNumberOnTheRight({ row: middleRow, index: i })
-            );
+        if (element === "*") {
+          const upRowIndexes = [];
+          upRowIndexes.push(
+            ...checkIfNumberOnTheLeft({ row: upRow, index: i })
+          );
+          upRowIndexes.push(
+            ...checkIfNumberOnTheRight({ row: upRow, index: i })
+          );
+          const sortedUpRowIndexes = removeDuplicatesAndSort(upRowIndexes);
+          const arrayOfIndexesOfUpRowNumbers =
+            mapSubArraysOfIndexes(sortedUpRowIndexes);
+          const upRowNumbers = mapNumbersFromArray(
+            arrayOfIndexesOfUpRowNumbers,
+            upRow.split("")
+          );
+          const middleRowIndexes = [];
+          middleRowIndexes.push(
+            ...checkIfNumberOnTheRight({ row: middleRow, index: i })
+          );
+          middleRowIndexes.push(
+            ...checkIfNumberOnTheLeft({ row: middleRow, index: i })
+          );
+          const sortedMiddleRowIndexes =
+            removeDuplicatesAndSort(middleRowIndexes);
+          const arrayOfIndexesOfMiddleRowNumbers = mapSubArraysOfIndexes(
+            sortedMiddleRowIndexes
+          );
+          const middleRowNumbers = mapNumbersFromArray(
+            arrayOfIndexesOfMiddleRowNumbers,
+            middleRow.split("")
+          );
+          const downRowIndexes = [];
+          downRowIndexes.push(
+            ...checkIfNumberOnTheRight({ row: downRow, index: i })
+          );
+          downRowIndexes.push(
+            ...checkIfNumberOnTheLeft({ row: downRow, index: i })
+          );
+          const sortedDownRowIndexes = removeDuplicatesAndSort(downRowIndexes);
+          const arrayOfIndexesOfDownRowNumbers =
+            mapSubArraysOfIndexes(sortedDownRowIndexes);
+          const downRowNumbers = mapNumbersFromArray(
+            arrayOfIndexesOfDownRowNumbers,
+            downRow.split("")
+          );
+          const resultArray = [
+            ...upRowNumbers,
+            ...middleRowNumbers,
+            ...downRowNumbers,
+          ].filter((element) => !isNaN(element));
+          if (resultArray.length > 1) {
+            result += resultArray.reduce((a, b) => a * b, 1);
           }
         }
-      });
-      const sortedFilteredIndexes = Array.from(
-        new Set<number>(arrayOfIndexesWhereNumber.sort((a, b) => a - b))
-      );
-      let subarrays = [];
-      let currentSubarray = [sortedFilteredIndexes[0]];
-
-      for (let i = 1; i < sortedFilteredIndexes.length; i++) {
-        if (
-          Math.abs(
-            sortedFilteredIndexes[i] -
-              currentSubarray[currentSubarray.length - 1]
-          ) === 1
-        ) {
-          currentSubarray.push(sortedFilteredIndexes[i]);
-        } else {
-          subarrays.push(currentSubarray);
-          currentSubarray = [sortedFilteredIndexes[i]];
-        }
-      }
-
-      subarrays.push(currentSubarray);
-
-      subarrays.forEach((subarray) => {
-        let str = "";
-        subarray.forEach((element) => {
-          str += middleRow[element];
-        });
-        result += parseInt(str);
       });
     }
     console.log(result);
